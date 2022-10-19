866e68614603700f4cdd3832a8ca94b78a9ac019
Mateus <mateus.gotardi@gmail.com>
2022-10-19 01:38:59 -0300

    socket server conectando

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-require('dotenv').config()
-const app = require('../server.js')
-const mongoose = require("mongoose")
+require("dotenv").config();
+const { Server } = require("socket.io");
+const server = require("../server.js");
+const mongoose = require("mongoose");
+var rooms = {};
 mongoose.connect(process.env.MONGO_URL, () => {
-  console.log('connected to mongoDB')
-})
-app.listen(process.env.PORT, ()=>{console.log(`listening on port ${process.env.PORT}`)})
-module.exports = app
+  console.log("connected to mongoDB");
+});
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+server.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+});
+const createRoom = (id)=>{
+  rooms[id]={a:'sala criada'}
+  console.log(rooms)
+}
+io.on("connection", async (socket) => {
+  console.log("socket connected:" + socket.id);
+  socket.on("connect_error", (err) => {
+    console.log(err.message);
+  });
+
+  socket.on("join-room", async (command) => {
+    let roomID = command.roomID;
+    if (!rooms[roomID]) {
+      createRoom(roomID);
+      io.emit('joined-room', roomID)
+    }
+    else {
+      io.emit('joined-room', roomID)
+    }
+  });
+});
+
+module.exports = io;
